b069281d3bbacade6107bfcda3260362df962085
vincentGuerlais <vincent.guerlais@free.fr>
2026-08-07 14:33:55 +0200

    Create service-worker.js

---
+++ loot-tracker/service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "loot-tracker-v1";
+
+
+const FILES = [
+
+    "./",
+
+    "./index.html",
+
+    "./app.js",
+
+    "./style.css",
+
+    "./service-worker.js",
+
+    
+
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES))
+
+    );
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+
+            );
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+
+    );
+
+});
