58c1fe9d628d0545fa8a4c859c51ca40fd3a755d
vincentGuerlais <vincent.guerlais@free.fr>
2026-08-07 14:23:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "raid-helper-v2";
+const CACHE_NAME = "raid-helper-home-v2";
 
 
 const FILES = [
@@ -13,45 +13,11 @@ const FILES = [
 
     "./service-worker.js",
 
-
-    // Raid Marker
-
-    "./marker/index.html",
-
-    "./marker/style.css",
-
-    "./marker/app.js",
-
-
-    // Loot Tracker (placeholder pour l'instant)
-
-    "./loot/index.html",
-
-
-    // Assets communs
-
     "./assets/icon-192.png",
 
-    "./assets/icon-512.png",
-
-    "./assets/square_blue.png",
-
-    "./assets/cross_red.png",
-
-    "./assets/circle_orange.png",
-
-    "./assets/triangle_green.png",
-
-    "./assets/diamond_purple.png",
-
-    "./assets/moon_grey.png",
-
-    "./assets/skull_white.png",
-
-    "./assets/star_yellow.png"
+    "./assets/icon-512.png"
 
 ];
-
 
 
 self.addEventListener("install", event => {
@@ -59,12 +25,12 @@ self.addEventListener("install", event => {
     event.waitUntil(
 
         caches.open(CACHE_NAME)
-
         .then(cache => cache.addAll(FILES))
 
     );
 
 });
+
 
 self.addEventListener("activate", event => {
 
@@ -73,9 +39,11 @@ self.addEventListener("activate", event => {
         caches.keys().then(keys => {
 
             return Promise.all(
+
                 keys
                 .filter(key => key !== CACHE_NAME)
                 .map(key => caches.delete(key))
+
             );
 
         })
@@ -84,12 +52,12 @@ self.addEventListener("activate", event => {
 
 });
 
+
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
         caches.match(event.request)
-
         .then(response => response || fetch(event.request))
 
     );
