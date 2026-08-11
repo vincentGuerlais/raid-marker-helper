3e6c66ecaf976fbe39221743af51edf0a81087f6
vincentGuerlais <vincent.guerlais@free.fr>
2026-08-11 17:26:08 +0200

    Update service-worker.js

--- mythic-plus-helper/service-worker.js
+++ mythic-plus-helper/service-worker.js
@@ -1,1 +1,72 @@
+const CACHE_NAME = "mythic-plus-helper-v1";
 
+
+const FILES = [
+
+    "./",
+
+    "./index.html",
+    "./app.js",
+    "./style.css",
+    "./contents.json",
+    "./service-worker.js"
+
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache =>
+            cache.addAll(FILES)
+        )
+
+    );
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+
+                .filter(
+                    key => key !== CACHE_NAME
+                )
+
+                .map(
+                    key => caches.delete(key)
+                )
+
+            );
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(
+            response =>
+                response || fetch(event.request)
+        )
+
+    );
+
+});
