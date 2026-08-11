4d9b3d2ce19838d6b5717be419d418b2bbc10199
vincentGuerlais <vincent.guerlais@free.fr>
2026-08-11 17:23:59 +0200

    Update app.js

--- mythic-plus-helper/app.js
+++ mythic-plus-helper/app.js
@@ -1,1 +1,19 @@
+const contentsIndexResponse =
+    await fetch("contents.json");
 
+const contentsIndex =
+    await contentsIndexResponse.json();
+
+contents = [];
+
+for (const entry of contentsIndex.contents) {
+
+    const response =
+        await fetch(entry.file);
+
+    const content =
+        await response.json();
+
+    contents.push(content);
+
+}
